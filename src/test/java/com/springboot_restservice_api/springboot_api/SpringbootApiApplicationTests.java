d41c5e0523f1e3c65b707afd30682422958dacc8
Juan Galvis <galvisjuanc@gmail.com>
2023-02-24 09:26:40 -0500

    Initial Commit

---
+++ src/test/java/com/springboot_restservice_api/springboot_api/SpringbootApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot_restservice_api.springboot_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
