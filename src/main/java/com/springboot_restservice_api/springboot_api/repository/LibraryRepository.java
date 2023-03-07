722e5131719d00605f11bdd287b11385c989dc84
Juan Galvis <galvisjuanc@gmail.com>
2023-03-07 08:44:23 -0500

    Understand CRUD operations in Java and Springboot

---
+++ src/main/java/com/springboot_restservice_api/springboot_api/repository/LibraryRepository.java
@@ -0,0 +1,11 @@
+package com.springboot_restservice_api.springboot_api.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.springboot_restservice_api.springboot_api.entity.Library;
+
+@Repository
+public interface LibraryRepository extends JpaRepository <Library, String>{
+    
+}
