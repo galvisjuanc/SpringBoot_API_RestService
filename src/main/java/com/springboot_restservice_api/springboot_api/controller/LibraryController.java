4de716f4eb0e87c582378dddb962775d0449b9bf
Juan Galvis <galvisjuanc@gmail.com>
2023-03-08 09:38:05 -0500

    "Add validation to avoid duplicate records"

--- src/main/java/com/springboot_restservice_api/springboot_api/controller/LibraryController.java
+++ src/main/java/com/springboot_restservice_api/springboot_api/controller/LibraryController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.springboot_restservice_api.springboot_api.entity.AddResponse;
 import com.springboot_restservice_api.springboot_api.entity.Library;
 import com.springboot_restservice_api.springboot_api.repository.LibraryRepository;
+import com.springboot_restservice_api.springboot_api.service.LibraryService;
 
 @RestController
 public class LibraryController {
@@ -18,23 +19,33 @@ public class LibraryController {
     @Autowired
     LibraryRepository libraryRepository;
 
+    @Autowired
+    LibraryService libraryService;
+
     @PostMapping("/addBook")
     public ResponseEntity<AddResponse> addBookImplementation(@RequestBody Library library){
         
-        String idBook = library.getIsbn() + library.getAisle();
         AddResponse addResponse = new AddResponse();
         HttpHeaders httpHeaders = new HttpHeaders();
+        String idBook = libraryService.buildId(library.getIsbn(),library.getAisle());
+        libraryService.checkBookAlreadyExists(idBook);
 
-        library.setId(idBook);
-        libraryRepository.save(library);
-
-        addResponse.setMsg("Book was added successfully");
-        addResponse.setId(idBook);
-
-        httpHeaders.add("unique", idBook);
-        
-        return new ResponseEntity<AddResponse>(addResponse, httpHeaders, HttpStatus.CREATED);
-
+        if (!libraryService.checkBookAlreadyExists(idBook)){    
+            library.setId(idBook);
+            libraryRepository.save(library);
+    
+            addResponse.setMsg("Book was added successfully");
+            addResponse.setId(idBook);
+    
+            httpHeaders.add("unique", idBook);
+            
+            return new ResponseEntity<AddResponse>(addResponse, httpHeaders, HttpStatus.CREATED);
+        }
+        else {
+            addResponse.setMsg("Book already exists");
+            addResponse.setId(idBook);
+            return new ResponseEntity<AddResponse>(addResponse, HttpStatus.ACCEPTED);
+        }
     }
     
 }
---
+++ src/main/java/com/springboot_restservice_api/springboot_api/service/LibraryService.java
@@ -0,0 +1,26 @@
+package com.springboot_restservice_api.springboot_api.service;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.springboot_restservice_api.springboot_api.entity.Library;
+import com.springboot_restservice_api.springboot_api.repository.LibraryRepository;
+
+@Service
+public class LibraryService {
+
+    @Autowired
+    LibraryRepository libraryRepository;
+
+    public String buildId(String isbn, int aisle) {
+        return isbn+aisle;
+    }
+
+    public boolean checkBookAlreadyExists(String id) {
+
+        Optional<Library> libraryExists = libraryRepository.findById(id);
+        return libraryExists.isPresent() ? true : false;
+    }
+}
