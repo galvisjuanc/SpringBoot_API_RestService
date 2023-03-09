d45473598cb00728ef17e283568f8e2792ca32d9
Juan Galvis <galvisjuanc@gmail.com>
2023-03-09 08:42:34 -0500

    GetMapping created to get Book

--- src/main/java/com/springboot_restservice_api/springboot_api/controller/LibraryController.java
+++ src/main/java/com/springboot_restservice_api/springboot_api/controller/LibraryController.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -46,6 +48,12 @@ public class LibraryController {
             addResponse.setId(idBook);
             return new ResponseEntity<AddResponse>(addResponse, HttpStatus.ACCEPTED);
         }
+    }
+
+    @GetMapping("/getBooks/{id}")
+    public Library getBookById(@PathVariable(value="id") String id) {
+        Library libraryFound = libraryRepository.findById(id).get();
+        return libraryFound;
     }
     
 }
