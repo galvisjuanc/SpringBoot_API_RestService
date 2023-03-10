f9b39bc895a31cf03844897c098be1fe4fe38f17
Juan Galvis <galvisjuanc@gmail.com>
2023-03-10 09:11:06 -0500

    DeleteBook Api Created

--- src/main/java/com/springboot_restservice_api/springboot_api/controller/LibraryController.java
+++ src/main/java/com/springboot_restservice_api/springboot_api/controller/LibraryController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -85,4 +86,12 @@ public class LibraryController {
         return new ResponseEntity<Library>(libraryFoundToBeUpdated,HttpStatus.OK);
     }
     
+    @DeleteMapping("/deleteBook")
+    public ResponseEntity<String> deleteBookById(@RequestBody Library library) {
+
+        Library libraryToBeDeleted = libraryRepository.findById(library.getId()).get();
+        libraryRepository.delete(libraryToBeDeleted);
+        return new ResponseEntity<>("Book was deleted successfully", HttpStatus.CREATED);
+    }
+
 }
